ab5ec2c6cf1425cf5584c374d9b582102d38c60a
Faisal Ahmed <feisala132@gmail.com>
2025-04-16 21:08:51 +0100

    add ch1 and ch2 and ch3

---
+++ CH3/scripts.js
@@ -0,0 +1,35 @@
+/*
+
+A = P(1 + / r n/t)**nt
+
+plan
+
+figure out how to do exponents in js
+
+ */
+
+let AskPrincipleAmount = prompt('What is the Principle amount? ');
+let PrincipleAmount = parseInt(AskPrincipleAmount)
+
+
+let AskRate = prompt('What is the Rate? # give percentage number');
+let Rate = parseFloat(AskRate)
+
+let RateDiv = (Rate / 100)  // try without dividing see how wrong the number is 
+
+
+let AskNumOfYrs = prompt('What is the number of years? ');
+let NumOfYrs = parseInt(AskNumOfYrs)
+
+let AskNumOfInterest = prompt('What is the number of times the interest is compounded per year? ');
+let NumOfInterest = parseInt(AskNumOfInterest)
+
+
+let Interest = (PrincipleAmount * (1 + (RateDiv / NumOfInterest)) ** (NumOfInterest * NumOfYrs)).toFixed(2)
+alert(Interest)
+
+
+
+
+
+
